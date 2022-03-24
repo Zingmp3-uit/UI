d65a5c2c54c45d62e718825182c2f2966afe9d23
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-03-24 23:25:41 +0700

    config route with const component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,60 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import React from 'react';
 import Home from '../src/pages/Home'
 import Singer from '../src/pages/Singer'
 import Album from '../src/pages/Album'
 import MusicVideo from '../src/pages/MusicVideo'
+
+// const component
+import Header from '../src/components/ConstComponent/Header'
+import SideBar from '../src/components/ConstComponent/SideBar'
+import PlayMusic from '../src/components/ConstComponent/PlayMusic'
+
+
+const configComponent = (component) => {
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <div>
+                    <SideBar />
+                </div>
+                <div>
+                    {component}
+                </div>
+            </div>
+            <PlayMusic />
+        </div>
+    )
+}
+let path = [
+    {
+        path: '/singer',
+        component: configComponent(<Singer />)
+    },
+    {
+        path: '/album',
+        component: configComponent(<Album />)
+    },
+    {
+        path: '/music-video',
+        component: configComponent(<MusicVideo />)
+    },
+]
 
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/singer" element={<Singer />} />
-            <Route path="/album" element={<Album />} />
-            <Route path="/music-video" element={<MusicVideo />} />
+            {path.map((item, index) => {
+                return (
+                    <Route path={item.path} element={item.component} key={index} />
+                )
+            })}
         </Routes>
     );
 }
---
+++ src/components/ConstComponent/PlayMusic/index.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+const PlayMusic = () => {
+    
+
+    return (
+        <>
+        <h1>PlayMusic</h1>
+        </>
+    )
+}
+export default PlayMusic;
