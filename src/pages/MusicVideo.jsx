2fae4c13c8cc8c39a6130446e6a5846024b3df42
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-04-05 15:52:16 +0700

    add anchor bottom mp3

--- src/pages/MusicVideo.jsx
+++ src/pages/MusicVideo.jsx
@@ -11,6 +11,9 @@ import YouTube from 'react-youtube';
 import ItemMusicVideo from '../components/ItemMusicVideo';
 import ItemVideo from '../components/ItemVideo'
 
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
 
 const MusicVideo = () => {
     const [idVideo, setIdVideo] = useState('j5A461KptQ0')
@@ -54,7 +57,7 @@ const MusicVideo = () => {
                         ></iframe>
                     </div>
                     <div className='col-span-1 p-2  w-full h-[500px]'>
-                        <ItemMusicVideo idVideo={idVideo} setIdVideo={setIdVideo}/>
+                        <ItemMusicVideo idVideo={idVideo} setIdVideo={setIdVideo} />
                     </div>
                 </div>
             </div>
@@ -62,6 +65,14 @@ const MusicVideo = () => {
                 <ItemVideo />
             </div>
             <br />
+            <div>
+                <AudioPlayer
+                    autoPlay
+                    src={'https://vnso-zn-23-tf-mp3-s1-m-zmp3.zadn.vn/ea859fb645f2acacf5e3/2153293163773944052?authen=exp=1649319583~acl=/ea859fb645f2acacf5e3/*~hmac=178f8c944edd204ec6aff0f783bc6d10&fs=MTY0OTE0NjmUsIC4MzMyMHx3ZWJWNHwxMTMdUngMTYxLjY2LjEwMA'}
+                    onPlay={e => console.log("onPlay")}
+                // other props here
+                />
+            </div>
             <div>
                 <p>hi</p>
                 <p>hi</p>
