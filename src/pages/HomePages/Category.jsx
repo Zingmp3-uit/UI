7a8ff64900509e9d2290fc3d9d324755a395fa2a
Nguyen Tri Minh <minh.ngntri@gmail.com>
2022-04-12 14:50:26 +0700

    Update Category.jsx

--- src/pages/HomePages/Category.jsx
+++ src/pages/HomePages/Category.jsx
@@ -6,7 +6,7 @@ import React, { useState } from 'react'
 const Category = () => {
     return (
         <div className="w-full h-screen bg-contain bg-no-repeat bg-[url('https://photo-zmp3.zmdcdn.me/cover/c/0/2/b/c02b9e121d335e26b6537772b8d88faa.jpg')]">
-            <div className='absolute top-[350px] w-full h-full bg-black'>
+            <div className='absolute top-[400px] w-full h-full'>
             </div>
         </div>
     )
