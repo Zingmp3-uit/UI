c7aa5efa1d2a06d7884cb62f70c8271b0a1e05cc
Nguyen Tri Minh <minh.ngntri@gmail.com>
2022-04-16 21:06:32 +0700

    Update top suggested songs with API
    Each singer's name cannot be hovered yet.

--- src/pages/HomePages/Category.jsx
+++ src/pages/HomePages/Category.jsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+//API
+import ZingAPI from "../../context/zing.context";
+//MUI
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 
 const Category = () => {
-    const[itemHighlights, setItemHighlights] = useState([
-        {
-            id: 1,
-            title: "V-Pop 2022 Nghe Gì?",
-            artists: "Hoàng Thùy Linh, AMEE, ERIK★...",
-            thumb: "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/3/1/c/4/31c4e51e9541df4b259c52c384335f21.jpg"
-        },
-        {
-            id: 2,
-            title: "Today's V-Pop Hits",
-            artists: "MIN★, ERIK★, Hoàng Thùy Linh...",
-            thumb: "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/6/a/d/d/6add2c7faa4509922a4e8b4090fb83c0.jpg"
-        },
-        {
-            id: 3,
-            title: "Đỉnh Cao Trending",
-            artists: "ERIK★, Jack, Hoàng Dũng...",
-            thumb: "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/5/5/e/6/55e6a6d866da0d1badf7b07de95af598.jpg"
-        },
-        {
-            id: 4,
-            title: "Women Of V-Pop",
-            artists: "Hoàng Thùy Linh, Tóc Tiên, Đông Nhi...",
-            thumb: "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/6/d/6/9/6d691744b3560de085223bc0bae8f72f.jpg"
-        },
-    ])
+    const api = new ZingAPI();
+
+    const[itemsTopSuggest, setItemsTopSuggest] = useState([]);
+
+    const getapi = async () => {
+        api.search("Nhạc Việt").then(res => {
+            const randomItemsTopSuggest = shuffle(res.data.data.topSuggest).slice(0, 4);
+            setItemsTopSuggest(randomItemsTopSuggest);
+            console.log(randomItemsTopSuggest);
+        })
+    } 
+
+    function shuffle (arr) {
+        var j, x, index;
+        for (index = arr.length - 1; index > 0; index--) {
+            j = Math.floor(Math.random() * (index + 1));
+            x = arr[index];
+            arr[index] = arr[j];
+            arr[j] = x;
+        }
+        return arr;
+    }
+
+    useEffect(async () => {
+        getapi();
+    }, []);
 
     return (
         <div className='relative w-full h-screen'>
@@ -43,16 +46,16 @@ const Category = () => {
 
                 <div className='pt-[1.5%] grid grid-cols-4 gap-[4%]'> 
                 {
-                    itemHighlights.map((item, index) => {
+                    itemsTopSuggest.map((item, index) => {
                         return (
                             <div className='w-[100.5%] h-full'>
                                 <div className='relative group rounded-[3%] overflow-hidden'>
-                                    <img className='group-hover:scale-110 group-hover:brightness-50 duration-500 object-cover rounded-[3%]' src={item.thumb}>
+                                    <img className='group-hover:scale-110 group-hover:brightness-50 duration-500 object-cover rounded-[3%]' src={item.thumbnailM}>
                                     </img>
 
                                     <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] hidden group-hover:flex flex-row justify-around items-center">
                                         <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>
-                                        <div className="p-[35%] items-center justify-center">
+                                        <div className="p-[40%] items-center justify-center">
                                             <div className='pt-[18%] pl-[20%] h-12 w-12 border rounded-full'>
                                                 <PlayArrowIcon></PlayArrowIcon>
                                             </div>
@@ -61,12 +64,14 @@ const Category = () => {
                                     </div>                             
                                 </div>
 
-                                <div className='pt-[1.3%] font-bold text-[12pt]'>
+                                <div className='pt-[2%] font-bold text-[12pt] hover:text-[#c662ef] hover:cursor-pointer'>
                                     {item.title}
                                 </div>
 
-                                <div className='pt-[1.3%] text-zinc-500 font-semibold text-[10pt]'>
-                                    {item.artists}
+                                <div className='pt-[2%] text-zinc-500 font-semibold text-[11pt]'>
+                                {
+                                    (item.artists[0].spotlight == true ? item.artists[0].name + "★" : item.artists[0].name) + ", " + (item.artists[1].spotlight == true ? item.artists[1].name + "★" : item.artists[1].name) + ", " + (item.artists[2].spotlight == true ? item.artists[2].name + "★" : item.artists[2].name) + "..."
+                                }
                                 </div>
                             </div>
                         )
