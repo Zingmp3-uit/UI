9aa4726fc70a9eccdaa8345f7ce01b54e5404bc7
Gnol0909 <sieutromlong1412@gmail.com>
2022-05-12 21:00:40 +0700

    update#4

--- src/components/ItemArtists/ItemArtists.jsx
+++ src/components/ItemArtists/ItemArtists.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 //MUI
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import ShuffleRoundedIcon from '@material-ui/icons/ShuffleRounded';
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 
 function ItemArtists(props) {
     const artistList = props.artistList;
-    const isSubtitleClickable = props.isSubtitleClickable;
+    //const isSubtitleClickable = props.isSubtitleClickable;
 
     return (
     <div className='pt-[1.5%] grid grid-cols-4 gap-[4%]'>
@@ -15,23 +16,27 @@ function ItemArtists(props) {
             return (
                 <div className='w-full h-full'>
                     <div className='relative group rounded-[3%] overflow-hidden'>
-                        <img className='group-hover:scale-110 group-hover:brightness-50 duration-500 object-cover rounded-[3%]' src={item.thumbnailM}>
+                        <img className='group-hover:scale-110 group-hover:brightness-50 duration-500 object-cover rounded-full' src={item.thumbnailM}>
                         </img>
 
                         <div className="absolute p-[8px] w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] hidden group-hover:flex flex-row justify-around items-center">
-                            <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>
+                            
                             <div className="w-12 h-12 flex items-center justify-center border rounded-full">
-                                <PlayArrowIcon></PlayArrowIcon>
+                                <ShuffleRoundedIcon></ShuffleRoundedIcon>
                             </div>
-                            <MoreHorizOutlinedIcon></MoreHorizOutlinedIcon>                                       
+                                                                
                         </div>                             
                     </div>
 
-                    <div className='pt-[2%] font-bold text-[12pt] truncate ... hover:text-[#B1D0E0] hover:cursor-pointer'>
-                        {item.title}
+                    <div className='pt-[2%] font-bold text-[12pt] text-center  truncate ... hover:text-[#B1D0E0] hover:cursor-pointer'>
+                        {item.name}
+                    </div>
+                    <div className='pt-[2%] font-bold text-[9pt] text-center  truncate ... hover:text-[#B1D0E0] hover:cursor-pointer'>
+                        {(item.totalFollow % 1000) + 'K quan tâm'}
                     </div>
 
-                    {
+
+                    {/* {
                         isSubtitleClickable == true ?
                         <div className='pt-[2%] text-slate-400 font-semibold text-[11pt]'>
                             <a className='hover:text-[#B1D0E0] hover:underline hover:cursor-pointer' href={item.artists[0].alias}>
@@ -51,7 +56,7 @@ function ItemArtists(props) {
                         <div className='pt-[2%] text-slate-400 font-semibold text-[11pt]'>
                             {item.releaseDateText}
                         </div>
-                    }
+                    } */}
                 </div>
             )
         })
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -13,22 +13,29 @@ import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutline
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 //import AlbumLoader from "../components/SkeletonLoader/AlbumLoader";
 import ItemAlbum from '../components/ItemAlbum/ItemAlbum';
+import ItemArtists from '../components/ItemArtists/ItemArtists';
 
 const Album = () => {
     const api = new ZingAPI();
     const[itemsTopAlbum, setItemsTopAlbum] = useState([]);
     const[itemsTopSuggest, setItemsTopSuggest] = useState([]);
+    const[itemsTopSuggest1, setItemsTopSuggest1] = useState([]);
     const[itemsHotSongs, setItemsHotSongs] = useState([]);
     const[itemsArtist, setItemsArtist, ] = useState([]);
     const getapi = async () => {
         await api.search("Nhạc Việt").then(res => {
             const randomItemsTopAlbum = (res.data.data.topSuggest).slice(0, 1);
             setItemsTopAlbum(randomItemsTopAlbum);
-            //console.log(itemsTopAlbum);
+            console.log(itemsTopSuggest);
         });
         await api.search("Nhạc Việt").then(res => {
             const randomItemsTopSuggest = (res.data.data.topSuggest).slice(0, 4);
             setItemsTopSuggest(randomItemsTopSuggest);
+            //console.log(res);
+        });
+        await api.search("Nhạc Việt").then(res => {
+            const randomItemsTopSuggest1 = (res.data.data.topSuggest).slice(5, 9);
+            setItemsTopSuggest1(randomItemsTopSuggest1);
             //console.log(res);
         });
 
@@ -36,25 +43,26 @@ const Album = () => {
             setItemsHotSongs(res.data.data.weekChart.vn.items.slice(0, 15));
             //console.log(res);
         });
-        // await api.getDetailPlaylist("ZFFICOO7").then(res => {
-        //     const itemartist1 = (res.data.data.artists).slice(0,4);
-        //     const itemArtists1=[];
-        //     itemartist1.map((item, index)=>{
-        //         return(
-        //             //console.log(item.name),
-        //             api.getArtist(item.name).then(res => {
-        //                 console.log(res.data)
-        //             })
-        //         )
-        //     })
+        await api.getDetailPlaylist("ZFFICOO7").then(res => {
+            const itemartist1 = (res.data.data.artists).slice(0,4);
+            const itemArtists1=[];
+            itemartist1.map((item, index)=>{
+                return(
+                    //console.log(item.name),
+                    api.getArtist(item.alias).then(res => {
+                        itemArtists1.push(res.data.data);
+                        //console.log(itemArtists1);
+                    })
+                )
+            })
             
-        //     //setItemsArtist(itemArtists1);
-        //     //console.log(itemsArtist);
-        // });
-        await api.getArtist('Hoàng Thùy Linh').then(res => {
-            
-            console.log(res);
+            setItemsArtist(itemArtists1);
+            console.log(itemsArtist);
         });
+        // await api.getArtist('Hoàng Thùy Linh').then(res => {
+            
+        //     console.log(res);
+        // });
     }
 
     useEffect(async () => {
@@ -69,153 +77,8 @@ const Album = () => {
         return res;
     }
      
-    // const [itemArtists, setItemArtists] = useState([
-    //     {
-    //         id: 1,
-    //         name: 'NCT',
-    //         image_url: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/avatars/9/e/e/e/9eee7e94d4e1530370067c2d7bac929f.jpg'
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'NCT 127',
-    //         image_url: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/avatars/0/9/4/5/094502d5cae906eb9be005979edff647.jpg'
-    //     },
-    //     {
-    //         id: 3,
-    //         name: 'NCT U',
-    //         image_url: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/avatars/7/7/773932d903569e1dc72b2c850e3a8b21_1461308953.jpg'
-    //     },
-    //     {
-    //         id: 4,
-    //         name: 'EXO',
-    //         image_url: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/avatars/a/8/5/d/a85df1e32e6d116a794edb107c26c117.jpg'
-    //     }
-    // ])
-    const [itemSongs, setItemSongs] = useState([
-        {
-            id: 1,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 1
-        },
-        {
-            id: 2,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 2
-        },
-        {
-            id: 3,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 3
-        },
-        {
-            id: 4,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 4
-        },
-        {
-            id: 5,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 5
-        },
-        {
-            id: 6,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 6
-        },
-        {
-            id: 7,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 7
-        },
-        {
-            id: 8,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 8
-        },
-        {
-            id: 9,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 9
-        },
-        {
-            id: 10,
-            name: "My everything",
-            artist: "NCT U",
-            album: "NCT RESONANCE Pt. 2 - The 2nd Album",
-            time: "03:41",
-            image_url: "https://photo-resize-zmp3.zadn.vn/w94_r1x1_webp/cover/8/a/0/5/8a05e1d07f1c88e7093eac159682a341.jpg",
-            rate: 10
-        }
-    ])
-    const [itemPlaylists, setItemPlaylists] = useState([
-        {
-            id: 1,
-            name_id: "Playlist #1",
-            name: "Tèo",
-            thumd: "https://photo-playlist-zmp3.zadn.vn/s3/user-playlist?src=HavwqN7EvKCI1oYSFOdq0rHDR9nlYQ431bHjq6s6eLWCMJhUPOgYN0G4QDaurwCBL0qxtcw5emyP17tOOiFn1596D80fbxXJ7mfpaJNExWvK2dE6ReFeKW5S9TPjrRXN15roWppLx547NI3LFDEzLb5PJDSxYjHIL5X8rZgTZ1O4L0w4CTRENriVMirXcu8OIbz9Xt2RX1WSNq_B9e7LJXvJ7fyasuOO7mbDpdI7WKaTM4wOTTU57nyE1SKsWebPJGLEXt_SmWq4L0FBECs2JKv00yiudO9D1q0QdcREWayE5H_EPiU0IbnEJSWynziSK4f0cpYNsWKB31ENVG&size=thumb/240_240"
-        },
-        {
-            id: 2,
-            name_id: "Playlist #2",
-            name: "Kim Liên",
-            thumd: "https://photo-playlist-zmp3.zadn.vn/s3/user-playlist?src=HavwqN7EvKCI1oYSFOdq0rHDR9nlYQ431bHjq6s6eLWCMJhUPOgYN0G4QDaurwCBL0qxtcw5emyP17tOOiFn1596D80fbxXJ7mfpaJNExWvK2dE6ReFeKW5S9TPjrRXN15roWppLx547NI3LFDEzLb5PJDSxYjHIL5X8rZgTZ1O4L0w4CTRENriVMirXcu8OIbz9Xt2RX1WSNq_B9e7LJXvJ7fyasuOO7mbDpdI7WKaTM4wOTTU57nyE1SKsWebPJGLEXt_SmWq4L0FBECs2JKv00yiudO9D1q0QdcREWayE5H_EPiU0IbnEJSWynziSK4f0cpYNsWKB31ENVG&size=thumb/240_240"
-        },
-        {
-            id: 3,
-            name_id: "Playlist #3",
-            name: "Keem",
-            thumd: "https://photo-playlist-zmp3.zadn.vn/s3/user-playlist?src=HavwqN7EvKCI1oYSFOdq0rHDR9nlYQ431bHjq6s6eLWCMJhUPOgYN0G4QDaurwCBL0qxtcw5emyP17tOOiFn1596D80fbxXJ7mfpaJNExWvK2dE6ReFeKW5S9TPjrRXN15roWppLx547NI3LFDEzLb5PJDSxYjHIL5X8rZgTZ1O4L0w4CTRENriVMirXcu8OIbz9Xt2RX1WSNq_B9e7LJXvJ7fyasuOO7mbDpdI7WKaTM4wOTTU57nyE1SKsWebPJGLEXt_SmWq4L0FBECs2JKv00yiudO9D1q0QdcREWayE5H_EPiU0IbnEJSWynziSK4f0cpYNsWKB31ENVG&size=thumb/240_240"
-        },
-        {
-            id: 4,
-            name_id: "4U - On Repeat",
-            name: "Zing MP3",
-            thumd: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/cover/1/2/1/f/121fde6fe3898bc64cf04b716ddbe590.jpg"
-        },
-        {
-            id: 5,
-            name_id: "4U - On Repeat",
-            name: "Zing MP3",
-            thumd: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/cover/1/2/1/f/121fde6fe3898bc64cf04b716ddbe590.jpg"
-        }
-        
-    ])
+    
+    
     return (
         <div>
             <div className="grid grid-cols-8 px-[40px]">
@@ -352,39 +215,13 @@ const Album = () => {
                         <h2 className="text-[30px] font-bold leading-9 mr-[16px]">Nghệ sĩ tham gia</h2>
                         
                     </div>
-                    {/* <div className="flex flex-row ">
-                        <ItemAlbum 
-                        albumsList={itemsArtist}
-                        isSubtitleClickable={true}
+                    <div className="flex flex-row ">
+                        <ItemArtists 
+                        artistList={itemsArtist}
                         >
-                        </ItemAlbum>
-                    </div> */}
-                    {/* <div className="flex flex-row my-6">
-                        {
-                            itemArtists.map((item, index) => {
-                                return (
-                                <div className="flex flex-col justify-center p-[16px] cursor-pointer">
-                                    <div className="group  w-44 h-44 rounded-full overflow-hidden relative">
-                                        <img className="w-full object-cover group-hover:scale-110 duration-500" src={item.image_url} alt="" />
-                                        <div className="p-[8px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border rounded-full hidden group-hover:flex">
-                                            <ShuffleRoundedIcon />
-                                        </div>
-                                    </div>
-                                    <h5 className="mt-[16px] text-base text-center hover:underline hover:text-[#7200a1] ">{item.name}</h5>
-                                    <div className=' bg-[#6998AB] justify-center w-[60%] rounded-full mt-[10%] ml-[20%]'>
-                                    <div className='flex w-full h-full justify-center items-center text-sm font-semibold truncate ...'>
-                                        
-
-                                        <div className='w-[5px]'></div>
-
-                                        PHÁT NHẠC
-                                    </div>
-                                </div>  
-                                </div>  )
-                            })
-                        }  
-                        
-                    </div> */}
+                        </ItemArtists>
+                    </div>
+                    
                 </div>
                 <div className="pt-[70px] pl-[60px] pr-[60px]">
                     <div className="flex flex-row justify-between">
@@ -418,7 +255,7 @@ const Album = () => {
                     </div>
                     <div className="flex flex-row">
                     <ItemAlbum 
-                        albumsList={itemsTopSuggest}
+                        albumsList={itemsTopSuggest1}
                         isSubtitleClickable={true}
                         >
                         </ItemAlbum>
