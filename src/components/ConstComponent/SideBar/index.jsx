f8f50b1ecb99bc09d200bde20662d58c82f290cf
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-05-18 11:47:49 +0700

    change logo

--- src/components/ConstComponent/SideBar/index.jsx
+++ src/components/ConstComponent/SideBar/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import logo from '../../../assets/Muzzi_Logo_Full.svg'
+import logo from '../../../assets/Muzzi_Logo_Full_Remade.svg'
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 //mui
@@ -68,7 +68,7 @@ const SideBar = () => {
 
     return (
         <div className="fixed top-0 bottom-0 left-0 w-[200px] shadow-xl bg-[#1A374D] text-white ">
-            <img src={'https://svgshare.com/i/gSG.svg?fbclid=IwAR0XkB3pYh0JSAF95X9jlWHCa1EaG26WPjG8T0eQnKcRF_8jW1nJS73Bm0E'} alt="" className='w-full text-center px-7 py-3' />
+            <img src={logo} alt="" className='w-full text-center px-7 py-3' />
             <div>
                 {titlePath.map((item, index) => (
                     <div key={index} >
